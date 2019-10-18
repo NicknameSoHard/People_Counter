40bc1f342f9e44d8d10f53fe765672ef473abd50
Grigoryi K <nicknamesohard@gmail.com>
2019-10-18 13:20:27 +0500

    Moved old versions into dir Pep8 fixed

--- People_Counter.py
+++ People_Counter.py
@@ -1,37 +1,36 @@
-## author HydroslidE
-import numpy as np
+import numpy
 import cv2
-from time import time
-from datetime import datetime.now
+import time
+from datetime import datetime
 import Person
 
-###############
-## Function ##
-##############
-# Write log for 1C.
-def Save_log(direction, date_now):
+
+def save_log(direction, now):
+    """
+    Write log for 1C.
+    """
     try:
         if direction == "up": # Direction
-            direct = ["1","2"]
+            direct = ["1", "2"]
         else:
-            direct = ["1","1"]
-#        date_now = datetime.now()
-        ms_now = str(int(time.time() / 1000)) # time in ms
-        log_file = open(date_now.strftime("%d%m%y%H") + ".txt", "a") # Filename
-        log_file.write( date_now.strftime("%d.%m.%Y") + "|" + ms_now + "|" + direct[0] + "|" + direct[1] + "|PCCapture|"+"0" + "|\n")
-        log_file.close();
+            direct = ["1", "1"]
+        ms_now = str(int(time.time() / 1000))
+        log_file = open(now.strftime("%d%m%y%H") + ".txt", "a") # Filename
+        log_file.write(f"{now.strftime('%d.%m.%Y')}|{ms_now}|{direct[0]}|{direct[1]}|PCCapture|0|")
+        log_file.close()
         return True
-    except:
+    except Exception:
         return False
 
-def nothing(x): # Trackpad function
-    pass # nothing to do
+def nothing(x):
+    """
+     Trackpad function. nothing to do
+    """
+    pass
 
-##########
-## Main ##
-##########
+
 # Exit counters
-cnt_up   = 0
+cnt_up = 0
 cnt_down = 0
 
 # Catch video
@@ -48,62 +47,53 @@ try:
     for line in setting_file:
         setting.append(int(line))
     setting_file.close()
-    if(len(setting) != 5 or min(setting) <= 0): # Raise exeption
-        raise
+    if len(setting) != 5 or min(setting) <= 0:
+        raise Exception
     print("Load setting from file")
-except:
+except Exception:
     setting = [30, 215, 252, 288, 192]
     print("Load default setting")
 
-"""
-Memo for settings:
-setting[0] - min threshold
-setting[1] - min object
-setting[2] - max object
-setting[3] - down line
-setting[4] - top line
-setting[5] - visible_frame # In this version of program is off
-"""
 # take frame width and height
 frame_width = cap.get(3)
 frame_height = cap.get(4)
 res = (frame_height * frame_width)
 # Calculate the min and max size of the object
 min_areaTH = res / 40
-print ('Min area Threshold', min_areaTH)
+print('Min area Threshold', min_areaTH)
 max_areaTH = res / 3
-print ('Max area Threshold', max_areaTH)
+print('Max area Threshold', max_areaTH)
 
 # Calculate program setting
 # Build line on 3\5 screen
 line_down = int(3 * (frame_height / 5))
-print ("Red line coord y:", str(line_down))
+print("Red line coord y:", str(line_down))
 # Calculate coordinate extreme points
-pt1 =  [0, line_down];
-pt2 =  [frame_width, line_down];
+pt1 = [0, line_down]
+pt2 = [frame_width, line_down]
 # Build and transform matrix
-pts_L1 = np.array([pt1, pt2], np.int32)
+pts_L1 = numpy.array([pt1, pt2], numpy.int32)
 pts_L1 = pts_L1.reshape((-1, 1, 2))
 line_down_color = (255, 0, 0)
 
 # Build line on 2\5 screen
 line_up = int(2*(frame_height / 5))
-print ("Blue line coord y:", str(line_up))
-pt3 =  [0, line_up];
-pt4 =  [frame_width, line_up];
-pts_L2 = np.array([pt3, pt4], np.int32)
+print("Blue line coord y:", str(line_up))
+pt3 = [0, line_up]
+pt4 = [frame_width, line_up]
+pts_L2 = numpy.array([pt3, pt4], numpy.int32)
 pts_L2 = pts_L2.reshape((-1, 1, 2))
 line_up_color = (0, 0, 255)
 
 # Create new array with 8-bit unsigned integer for morphological filters
-kernelOp = np.ones((3,3),np.uint8)
-kernelOp2 = np.ones((5,5),np.uint8)
-kernelCl = np.ones((11,11),np.uint8)
+kernelOp = numpy.ones((3, 3), numpy.uint8)
+kernelOp2 = numpy.ones((5, 5), numpy.uint8)
+kernelCl = numpy.ones((11, 11), numpy.uint8)
 
 # Make Hershey Fonts
 font = cv2.FONT_HERSHEY_SIMPLEX
 # Create panel and trackpads
-Panel = np.zeros([1,256], np.uint8)
+Panel = numpy.zeros([1, 256], numpy.uint8)
 cv2.namedWindow("Panel")
 
 cv2.createTrackbar("THRSH_MIN", "Panel", setting[0], 255, nothing)
@@ -111,20 +101,18 @@ cv2.createTrackbar("MIN OBJ", "Panel", setting[1], 255, nothing)
 cv2.createTrackbar("MAX OBJ", "Panel", setting[2], 255, nothing)
 cv2.createTrackbar("DWN_LINE", "Panel", setting[3], int(frame_height), nothing)
 cv2.createTrackbar("TOP_LINE", "Panel", setting[4], int(frame_height), nothing)
-#cv2.createTrackbar("VISIBLE_FRAME", "Panel", setting[5], 1, nothing)
-#cv2.resizeWindow("Panel", int(frame_height)+15, -5)
 # Makes array for detector
 persons = []
 iteration_counter = 0
-now_setting = [1,1,1,1,1]
+now_setting = [1, 1, 1, 1, 1]
 
 # Get mask
 ret, mask_frame = cap.read()
-while(cap.isOpened()):
+while cap.isOpened():
     # Get frame
     ret, frame = cap.read()
     # Calculates the absolute difference between mask and frame frames(arrays).
-    frame_ABS =  cv2.absdiff(frame, mask_frame)
+    frame_ABS = cv2.absdiff(frame, mask_frame)
 #    cv2.imshow('ADS',frame_ABS)
     # Converts an image to gray color space.
     gray_frame = cv2.cvtColor(frame_ABS, cv2.COLOR_BGR2GRAY)
@@ -136,11 +124,10 @@ while(cap.isOpened()):
     now_setting[2] = cv2.getTrackbarPos("MAX OBJ", "Panel")
     now_setting[3] = cv2.getTrackbarPos("DWN_LINE", "Panel")
     now_setting[4] = cv2.getTrackbarPos("TOP_LINE", "Panel")
-#    now_setting[5] = cv2.getTrackbarPos("VISIBLE_FRAME", "Panel")
 
     # if position trackbars not equal setting change and save setting
-    if not np.array_equal(now_setting, setting):
-        for i in range(0,5):
+    if not numpy.array_equal(now_setting, setting):
+        for i in range(0, 5):
             setting[i] = now_setting[i]
 
         # Recalculate setting
@@ -148,115 +135,112 @@ while(cap.isOpened()):
         max_areaTH = res / (255-setting[2]+1)
 
         # If setting line position change, recalculate and her
-        if (line_down != int(setting[3])):
+        if line_down != int(setting[3]):
             line_down = int(setting[3])
             print("Red line y:", str(line_down))
-            pt1 = [0, line_down];
-            pt2 = [frame_width, line_down];
-            pts_L1 = np.array([pt1, pt2], np.int32)
+            pt1 = [0, line_down]
+            pt2 = [frame_width, line_down]
+            pts_L1 = numpy.array([pt1, pt2], numpy.int32)
             pts_L1 = pts_L1.reshape((-1, 1, 2))
             line_down_color = (255, 0, 0)
 
-        if (line_up != int(setting[4])):
+        if line_up != int(setting[4]):
             line_up = int(setting[4])
             print("Blue line y:", str(line_up))
-            pt3 = [0, line_up];
-            pt4 = [frame_width, line_up];
-            pts_L2 = np.array([pt3, pt4], np.int32)
+            pt3 = [0, line_up]
+            pt4 = [frame_width, line_up]
+            pts_L2 = numpy.array([pt3, pt4], numpy.int32)
             pts_L2 = pts_L2.reshape((-1, 1, 2))
             line_up_color = (0, 0, 255)
 
         # Save setting on file
         setting_file = open("Setting", "w")
-        setting_file.write(str(setting[0]) +  "\n" + str(setting[1]) + "\n" + str(setting[2]) + "\n" + str(setting[3]) + "\n" + str(setting[4]))
-        setting_file.close();
+        for sett in setting:
+            setting_file.write(sett)
+        setting_file.close()
         print("Saved in file")
 
     try:
-        # Binarize the difference frame (Change color space to black and white)
-        ret,imBin= cv2.threshold(gray_frame, setting[0], 255, cv2.THRESH_BINARY)
-#        cv2.imshow('threshold',imBin)
+        # Threshold the difference frame (Change color space to black and white)
+        ret, imBin = cv2.threshold(gray_frame, setting[0], 255, cv2.THRESH_BINARY)
         # Morphology transformation (open and close). Just filter the noise in the image.
         morphology_mask = cv2.morphologyEx(imBin, cv2.MORPH_OPEN, kernelOp)
-        morphology_mask =  cv2.morphologyEx(morphology_mask , cv2.MORPH_CLOSE, kernelCl)
-        cv2.imshow('morphologyEx', morphology_mask )
-    except:
+        morphology_mask = cv2.morphologyEx(morphology_mask, cv2.MORPH_CLOSE, kernelCl)
+        cv2.imshow('morphologyEx', morphology_mask)
+    except Exception:
         # All exceptions end the program.
         print('Error threshold or morphology. End of programm.')
-        print ('UP:',cnt_up)
-        print ('DOWN:',cnt_down )
+        print(f"UP:{cnt_up}")
+        print(f"DOWN:{cnt_down}")
         break
 
-        # Find countours on image
-    ___, contours0, ___ = cv2.findContours(morphology_mask,
-        cv2.RETR_EXTERNAL, # Retrieves only the extreme outer contours
-        cv2.CHAIN_APPROX_SIMPLE # Extracts only edge points of contours
-    )
+        # Find contours on image
+    ___, contours0, ___ = cv2.findContours(
+                                morphology_mask,
+                                cv2.RETR_EXTERNAL,  # Retrieves only the extreme outer contours
+                                cv2.CHAIN_APPROX_SIMPLE  # Extracts only edge points of contours
+                                )
 
-    # For all countours
+    # For all contours
     for cnt in contours0:
-        # Calculate size of countour
+        # Calculate size of contours
         area = cv2.contourArea(cnt)
-        # Сheck cameras on noise
+        # check cameras on noise
         print(area, (frame_width * frame_height))
         if area > (frame_width * frame_height) * 0.90:
             iteration_counter = 1000
-        if area > min_areaTH and area < max_areaTH:
-            # Image moments help you to calculate some features like center of mass of the object. We calculate it.
+        if min_areaTH < area < max_areaTH:
             M = cv2.moments(cnt)
             # Get coordinates of the edges
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             # Get the coordinates of the scope
-            x,y,w,h = cv2.boundingRect(cnt)
+            x, y, w, h = cv2.boundingRect(cnt)
             # Check for a new object
             new = True
             for i in persons:
                 # If the object is close to already detected
-                if abs(cx-i.getX()) <= w and abs(cy-i.getY()) <= h:
-                    #print("Not new obj")
+                if abs(cx-i.getX()) <= w and abs(cy-i.get_y()) <= h:
                     new = False
                     # Update coordinates for better tracking
-                    i.updateCoords(cx,cy)
+                    i.update_coords(cx, cy)
 
                     date_now = datetime.now()
                     # Check cross the line
-                    if i.going_UP(line_down, line_up) == True:
-                        cnt_up += 1;
+                    if i.going_up(line_down, line_up):
+                        cnt_up += 1
                         # Write log to console and file
-                        print ("Person crossed going up at", str(date_now.strftime("%d.%m.%Y")) )
-                        if not Save_log("up", date_now):
+                        print(f"Person crossed going up at {date_now.strftime('%d.%m.%Y')}")
+                        if not save_log("up", date_now):
                             print("Logfile save error")
                         break
 
-                    elif i.going_DOWN(line_down, line_up) == True:
-                        cnt_down += 1;
-                        print ("Person crossed going down at", str(date_now.strftime("%d.%m.%Y")) )
-                        if not Save_log("down", date_now):
+                    elif i.going_down(line_down, line_up):
+                        cnt_down += 1
+                        print(f"Person crossed going down at {date_now.strftime('%d.%m.%Y')}")
+                        if not save_log("down", date_now):
                             print("Logfile save error")
                         break
                 else:
                     print("New obj")
                 # delete object if work done
-                if i.timedOut():
+                if i.timed_out():
                     index = persons.index(i)
                     persons.pop(index)
                     del i
 
             # Create new object
-            if new == True:
+            if new:
                 p = Person.MyPerson(cx, cy)
                 persons.append(p)
 
             # Draw red dot in a middle object, and make scope.
             cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
             cv2.rectangle(frame, (x, y),(x + w, y + h), (0, 255, 0), 2)
-            # cv2.drawContours(frame, cnt, -1, (0, 255, 0), 3) # And we can draw all object countours
             iteration_counter = 0
         else:
             # Count frames without tracking objects
-            iteration_counter +=1;
-            #print("Strange object!" + str(iteration_counter))
+            iteration_counter += 1
             # If we had a lot of frames without tracking object, we update mask and tracking array
             if iteration_counter > 1000:
                 ret, mask_frame = cap.read()
@@ -264,19 +248,18 @@ while(cap.isOpened()):
                 persons = []
             print(iteration_counter)
     # Draw lines and text on frames
-    str_up = 'UP: '+ str(cnt_up)
-    str_down = 'DOWN: '+ str(cnt_down)
-    frame = cv2.polylines(frame,[pts_L1], False, line_down_color, thickness = 2)
-    frame = cv2.polylines(frame,[pts_L2], False, line_up_color,thickness = 2)
+    str_up = f"UP: {cnt_up}"
+    str_down = f"DOWN:{cnt_down}"
+    frame = cv2.polylines(frame, [pts_L1], False, line_down_color, thickness=2)
+    frame = cv2.polylines(frame, [pts_L2], False, line_up_color, thickness=2)
 
     cv2.putText(frame, str_up, (10, 40), font, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(frame, str_up, (10, 40),font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+    cv2.putText(frame, str_up, (10, 40), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
     cv2.putText(frame, str_down, (10, 90), font, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.putText(frame, str_down, (10, 90), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
 
     # Show final frame
-    #cv2.imshow('Frame',frame)
-    cv2.imshow('Frame',frame)
+    cv2.imshow('Frame', frame)
 
     # Checking the key press
     k = cv2.waitKey(30) & 0xff
--- Person.py
+++ Person.py
@@ -1,43 +1,38 @@
-from random import randint
-import time
-
 class MyPerson:
-    tracks = []
     def __init__(self, xi, yi):
         self.x = xi
         self.y = yi
         self.tracks = []
         self.done = False
-        self.state = '0' # 0 -if object did not crossed the line, 1 - if yes
+        self.state = 0
         self.dir = None
 
-    def getTracks(self):
+    def get_tracks(self):
         return self.tracks
 
-    def getDir(self):
+    def get_dir(self):
         return self.dir
 
-    def getX(self):
+    def get_x(self):
         return self.x
 
-    def getY(self):
+    def get_y(self):
         return self.y
 
-    def updateCoords(self, xn, yn):
-        self.age = 0
-        self.tracks.append([self.x,self.y])
+    def update_coords(self, xn, yn):
+        self.tracks.append([self.x, self.y])
         self.x = xn
         self.y = yn
 
-    def timedOut(self):
+    def timed_out(self):
         return self.done
 
     # Check state and cross line
-    def going_UP(self,mid_start,mid_end):
+    def going_up(self, mid_start, mid_end):
         if len(self.tracks) >= 2:
-            if self.state == '0':
+            if not self.state:
                 if self.tracks[-1][1] < mid_end and self.tracks[-2][1] >= mid_end:
-                    self.state = '1'
+                    self.state = 1
                     self.dir = 'up'
                     return True
             else:
@@ -45,11 +40,11 @@ class MyPerson:
         else:
             return False
 
-    def going_DOWN(self,mid_start,mid_end):
+    def going_down(self, mid_start, mid_end):
         if len(self.tracks) >= 2:
-            if self.state == '0':
+            if not self.state:
                 if self.tracks[-1][1] > mid_start and self.tracks[-2][1] <= mid_start:
-                    self.state = '1'
+                    self.state = 1
                     self.dir = 'down'
                     return True
             else:
