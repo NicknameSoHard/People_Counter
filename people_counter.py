c51d2af2a27ab5b2f6f6a8722c7a4b471a0173b4
Grigoryi K <nicknamesohard@gmail.com>
2020-10-20 18:25:18 +0500

    Added .env.example Added russian README.md Restructed code

--- people_counter.py
+++ people_counter.py
@@ -2,11 +2,11 @@ import datetime
 
 import cv2
 import numpy
+import os
 
-
-from base_camera import Camera
-from person import Person
-from settingsdict import SettingsDict
+from utils.base_camera import Camera
+from utils.person import Person
+from utils.settings_dict import SettingsDict
 
 
 class PeopleCounter:
@@ -14,7 +14,7 @@ class PeopleCounter:
         self.cnt_up = 0
         self.cnt_down = 0
 
-        cam_string = 0  # "http://192.168.1.35//video.cgi"
+        cam_string = os.getenv('CAMERA_ADDRESS', 0)
         # Catch video
         self.cap = Camera(cam_string)
         # Load settings
@@ -29,11 +29,21 @@ class PeopleCounter:
 
         cv2.namedWindow("Panel")
 
-        cv2.createTrackbar("THRSH_MIN", "Panel", self.setting['THRSH_MIN'], 255, self.nothing)
-        cv2.createTrackbar("MIN_OBJ", "Panel", self.setting['MIN_OBJ'], 255, self.nothing)
-        cv2.createTrackbar("MAX_OBJ", "Panel", self.setting['MAX_OBJ'], 255, self.nothing)
-        cv2.createTrackbar("DWN_LINE", "Panel", self.setting['DWN_LINE'], self.cam_characteristic['frame_height'], self.nothing)
-        cv2.createTrackbar("TOP_LINE", "Panel", self.setting['TOP_LINE'], self.cam_characteristic['frame_height'], self.nothing)
+        cv2.createTrackbar("THRSH_MIN", "Panel", self.setting['THRSH_MIN'], 255, self.do_nothing)
+        cv2.createTrackbar("MIN_OBJ", "Panel", self.setting['MIN_OBJ'], 255, self.do_nothing)
+        cv2.createTrackbar("MAX_OBJ", "Panel", self.setting['MAX_OBJ'], 255, self.do_nothing)
+        cv2.createTrackbar("DWN_LINE", "Panel", self.setting['DWN_LINE'],
+                           self.cam_characteristic['frame_height'], self.do_nothing)
+        cv2.createTrackbar("TOP_LINE", "Panel", self.setting['TOP_LINE'],
+                           self.cam_characteristic['frame_height'], self.do_nothing)
+
+        # Create morphological filters
+        self.kernelOp = numpy.ones((3, 3), numpy.uint8)
+        self.kernelCl = numpy.ones((11, 11), numpy.uint8)
+
+        # create fonts type
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+
         # Makes array for detector
         self.persons = []
 
@@ -42,20 +52,17 @@ class PeopleCounter:
         Write log for 1C.
         """
         try:
-            if direction == "up":  # Direction
-                direction_mark = "2"
-            else:
-                direction_mark = "1"
+            direction_mark = "2" if direction == "up" else "1"
+
             now = datetime.datetime.now()
-#            ms_now = str(int(time.time() / 1000))
             with open(now.strftime("%d%m%y%H") + ".txt", "a") as log_file:
-                log_file.write(f"{now.strftime('%d.%m.%Y')}|{now.microsecond}|1|{direction_mark[1]}|PCCapture|0|")
-#            log_file.close()
+                log_file.write(f"{now.strftime('%d.%m.%Y')}|{now.microsecond}"
+                               f"|1|{direction_mark[1]}|PCCapture|0|")
             return True
         except Exception:
             return False
 
-    def nothing(x):
+    def do_nothing(x):
         """
          Trackpad function. nothing to do
         """
@@ -63,13 +70,6 @@ class PeopleCounter:
 
     def run(self):
         iteration_counter = 0
-
-        # Create new array with 8-bit unsigned integer for morphological filters
-        self.kernelOp = numpy.ones((3, 3), numpy.uint8)
-        self.kernelCl = numpy.ones((11, 11), numpy.uint8)
-
-        # Make Hershey Fonts
-        self.font = cv2.FONT_HERSHEY_SIMPLEX
 
         # Create panel and trackpads
         panel = numpy.zeros([1, 256], numpy.uint8)
@@ -82,7 +82,7 @@ class PeopleCounter:
             frame = self.cap.get_frame()
             # Calculates the absolute difference between mask and frame frames(arrays).
             frame_ABS = cv2.absdiff(frame, mask_frame)
-#            cv2.imshow('ADS',frame_ABS)
+            #            cv2.imshow('ADS',frame_ABS)
             # Converts an image to gray color space.
             gray_frame = cv2.cvtColor(frame_ABS, cv2.COLOR_BGR2GRAY)
 
@@ -97,18 +97,16 @@ class PeopleCounter:
 
             if settings_change:
                 # Recalculate setting
-                self.min_areaTH = self.res / (255-self.setting['MIN_OBJ']+1)
-                self.max_areaTH = self.res / (255-self.setting['MAX_OBJ']+1)
+                self.min_areaTH = self.res / (255 - self.setting['MIN_OBJ'] + 1)
+                self.max_areaTH = self.res / (255 - self.setting['MAX_OBJ'] + 1)
 
                 # If setting line position change, recalculate and her
                 line_down = int(self.setting[3])
-#                print("Red line y:", str(line_down))
                 pt1, pt2 = [0, line_down], [self.cam_characteristic['frame_width'], line_down]
                 pts_L1 = numpy.array([pt1, pt2], numpy.int32)
                 pts_L1 = pts_L1.reshape((-1, 1, 2))
 
                 line_up = int(self.setting[4])
-#                print("Blue line y:", str(line_up))
                 pt3, pt4 = [0, line_up], [self.cam_characteristic['frame_width'], line_up]
                 pts_L2 = numpy.array([pt3, pt4], numpy.int32)
                 pts_L2 = pts_L2.reshape((-1, 1, 2))
@@ -122,7 +120,7 @@ class PeopleCounter:
                 # Morphology transformation (open and close). Just filter the noise in the image.
                 morphology_mask = cv2.morphologyEx(imBin, cv2.MORPH_OPEN, self.kernelOp)
                 morphology_mask = cv2.morphologyEx(morphology_mask, cv2.MORPH_CLOSE, self.kernelCl)
-#                cv2.imshow('morphologyEx', morphology_mask)
+            #                cv2.imshow('morphologyEx', morphology_mask)
             except Exception:
                 # All exceptions end the program.
                 print('Error threshold or morphology. End of programm.')
@@ -131,31 +129,33 @@ class PeopleCounter:
                 break
 
                 # Find contours on image
-            ___, contours0, ___ = cv2.findContours(
-                                        morphology_mask,
-                                        cv2.RETR_EXTERNAL,  # Retrieves only the extreme outer contours
-                                        cv2.CHAIN_APPROX_SIMPLE  # Extracts only edge points of contours
-                                        )
+            ___, contours0, ___ = cv2.findContours(morphology_mask,
+                                                   cv2.RETR_EXTERNAL,
+                                                   cv2.CHAIN_APPROX_SIMPLE
+                                                   )
 
             # For all contours
             for cnt in contours0:
                 # Calculate size of contours
                 area = cv2.contourArea(cnt)
                 # check cameras on noise
-                if area > self.cam_characteristic['frame_width'] * self.cam_characteristic['frame_height'] * 0.90:
+                detected_area = self.cam_characteristic['frame_width'] \
+                                * self.cam_characteristic['frame_height'] * 0.90
+                if area > detected_area:
                     iteration_counter = 1000
+
                 if self.min_areaTH < area < self.max_areaTH:
                     M = cv2.moments(cnt)
                     # Get coordinates of the edges
-                    cx = int(M['m10']/M['m00'])
-                    cy = int(M['m01']/M['m00'])
+                    cx = int(M['m10'] / M['m00'])
+                    cy = int(M['m01'] / M['m00'])
                     # Get the coordinates of the scope
                     x, y, w, h = cv2.boundingRect(cnt)
                     # Check for a new object
                     new = True
                     for i in self.persons:
                         # If the object is close to already detected
-                        if abs(cx-i.getX()) <= w and abs(cy-i.get_y()) <= h:
+                        if abs(cx - i.getX()) <= w and abs(cy - i.get_y()) <= h:
                             new = False
                             # Update coordinates for better tracking
                             i.update_coords(cx, cy)
@@ -165,14 +165,12 @@ class PeopleCounter:
                             if i.going_up(line_up):
                                 self.cnt_up += 1
                                 # Write log to console and file
-                                if not self.save_log("up"):
-                                    print("Logfile save error")
+                                self.save_log("up")
                                 break
 
                             elif i.going_down(line_down):
                                 self.cnt_down += 1
-                                if not self.save_log("down"):
-                                    print("Logfile save error")
+                                self.save_log("down")
                                 break
                         else:
                             print("New obj")
